0dd149a196a52fda067459476bcc7b6bf3366e6d
ihriel mosko <ihriel.mosko@gmail.com>
2023-06-22 10:01:15 -0300

    html --> php

---
+++ Pages/Linguagens.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/linguagens.css">
+    <title>Linguagens</title>
+</head>
+<header>
+    <div class="container-elementos-header">
+        <div class="logo-container">
+            <a href="Página-inicial.php"><h1 class="up-text">Escola</h1></a>
+            <a href="Página-inicial.php"><p class="down-text">do Código</p></a>
+        </div>
+        <nav>
+            <ul class="lista-navegação">
+                <li class="cabecalho-link"><a href="Linguagens.php">Linguagens</a></li>
+                <li class="cabecalho-link"><a href="Tutoriais.php">Tutoriais</a></li>
+                <li class="cabecalho-link"><a href="Perfil.php">Perfil</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<section>
+<main class="conteudoPrincipal">
+    <div class="titulo-container">
+        <h1 class="subtitulo">Nossas Linguagens e Frameworks</h1>
+    </div>
+    <div class="info-liguagens">
+    </div>
+    <div class="conteudoPrincipal-container">
+        <nav>
+            <ul class="conteudoPrincipal-linguagens">
+                <li class="conteudoPrincipal-linguagens-link"><a href="#">HTML/CSS</a></li>
+                <li class="conteudoPrincipal-linguagens-link"><a href="#">PHP</a></li>
+                <li class="conteudoPrincipal-linguagens-link"><a href="#">JAVASCRIPT</a></li>
+                <li class="conteudoPrincipal-linguagens-link"><a href="#">EMBER JS</a></li>
+                <li class="conteudoPrincipal-linguagens-link"><a href="#">REACT</a></li>
+                <li class="conteudoPrincipal-linguagens-link"><a href="#">LARAVEL</a></li>
+            </ul>
+        </nav>
+        
+    </div>
+    <div class="container">
+        <div class="info html-css">
+         <h1 class="titulo">HTML E CSS</h1>
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+               at accusamus! Rerum!</p>
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+             Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+              at accusamus! Rerum!</p>
+         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+             Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+              at accusamus! Rerum!</p>
+         </div>
+         <div class="info php">
+             <h1 class="titulo">PHP</h1>
+             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                   at accusamus! Rerum!</p>
+             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                 Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                  at accusamus! Rerum!</p>
+             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                 Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                  at accusamus! Rerum!</p>
+             </div>
+             <div class="info js">
+                 <h1 class="titulo">JAVASCRIPT</h1>
+                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                       at accusamus! Rerum!</p>
+                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                     Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                      at accusamus! Rerum!</p>
+                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                     Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                      at accusamus! Rerum!</p>
+                 </div>
+                 <div class="info react">
+                     <h1 class="titulo">REACT</h1>
+                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                          Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                           at accusamus! Rerum!</p>
+                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                         Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                          at accusamus! Rerum!</p>
+                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                         Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                          at accusamus! Rerum!</p>
+                     </div>
+                     <div class="info emberjs">
+                         <h1 class="titulo">EMBER JS</h1>
+                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                              Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                               at accusamus! Rerum!</p>
+                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                             Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                              at accusamus! Rerum!</p>
+                         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                             Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                              at accusamus! Rerum!</p>
+                         </div>
+                         <div class="info laravel">
+                             <h1 class="titulo">LARAVEL</h1>
+                             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                  Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                                   at accusamus! Rerum!</p>
+                             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                 Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                                  at accusamus! Rerum!</p>
+                             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                                 Dolorum totam dolores consequatur corporis veniam velit perspiciatis vel iure impedit laborum cumque delectus quis aut assumenda sequi non,
+                                  at accusamus! Rerum!</p>
+                             </div>
+     </div>
+</main>
+</html>
