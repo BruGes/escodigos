2a3349eb8f3c4816be5a816f434c3c3a4e6b4c80
BruGes <brunogui244@gmail.com>
2023-06-26 23:42:27 -0300

    ++

--- Pages/validation.php
+++ Pages/validation.php
@@ -1,11 +1,12 @@
-<?php include "config.php";?>
+
 <?php
 
 if (isset($_POST['logar'])) {
     $email = $_POST['email-login'];
     $senha = $_POST['Senha-login'];
     $email2;
-    $consulta = $conn->prepare('SELECT * FROM `usuario` WHERE `ds_email` == :emaill AND `vl_senha` == :senhal');
+    include "config.php";
+    $consulta = $conn->prepare('SELECT * FROM `usuario` WHERE `ds_email` = :emaill AND `vl_senha` = :senhal');
     $consulta->bindValue(':emaill',$email);
     $consulta->bindValue(':senhal',$senha);
     $consulta->execute();
