eafd5c9d541224281b0d02ed25a1bdf6e1b6783a
ihriel mosko <ihriel.mosko@gmail.com>
2023-06-26 21:39:36 -0300

    validação de senha no cadastro

--- Pages/Cadastro.php
+++ Pages/Cadastro.php
@@ -9,12 +9,15 @@
         $nome = $_POST['nome'];
         $email = $_POST['email'];
         $senha = $_POST['senha'];
+        $senha2 = $_POST['senha2'];
 
-        $submit = $conn->prepare('INSERT INTO `usuario` (`id_usuario`, `nm_nome`, `ds_email`, `vl_senha`) VALUES (NULL, :nome, :email, :senha)');
-        $submit->bindValue(':nome', $nome);
-        $submit->bindValue(':email', $email);
-        $submit->bindValue(':senha', $senha);
-        $submit->execute();
+        if ($senha == $senha2) {
+            $submit = $conn->prepare('INSERT INTO `usuario` (`id_usuario`, `nm_nome`, `ds_email`, `vl_senha`) VALUES (NULL, :nome, :email, :senha)');
+            $submit->bindValue(':nome', $nome);
+            $submit->bindValue(':email', $email);
+            $submit->bindValue(':senha', $senha);
+            $submit->execute();
+        }
     }
 ?>
 
