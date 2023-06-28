c7925f6be0c7cc02e281ad907941faf9900d3687
ihriel mosko <ihriel.mosko@gmail.com>
2023-06-28 05:05:25 -0300

    tutorial 2

--- Pages/vscode-tuto1.php
+++ Pages/vscode-tuto1.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/tutoriai-style.css">
+    <link rel="stylesheet" href="../css/tutorial-style.css">
     <title>VSCode Tutorial 1</title>
 </head>
 <header>
---
+++ Pages/vscode-tuto2.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/tutorial-style.css">
+    <title>VSCode Tutorial 1</title>
+</head>
+<header>
+    <div class="container-elementos-header">
+        <div class="logo-container">
+            <a href="../index.php"><h1 class="up-text">Escola</h1></a>
+            <a href="../index.php"><p class="down-text">do Código</p></a>
+        </div>
+        <nav>
+            <ul class="lista-navegação">
+                <li class="cabecalho-link"><a href="Linguagens.php">Linguagens</a></li>
+                <li class="cabecalho-link"><a href="Tutoriais.php">Tutoriais</a></li>
+                <li class="cabecalho-link"><a href="Perfil.php">Perfil</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+<img src="assets/hheart.png" class="i-heart">
+<main class="text-container">
+    <div class="title">
+        <h1 class="title-l">CRIANDO E ABRINDO PROJETOS</h1>
+    </div>
+    <div class="text-main">
+        <p>Existem algumas formas de se abrir ou criar um novo projeto no vscode, abordaremos algumas delas</p>
+        </br>
+        <P>Para abrir um novo projeto, clique no símbolo de arquivo na barra lateral direita</P>
+        <img src="assets/t2-img1.png" class="imgs-t">
+        <p>Assim você abrirá o explorador de arquivos do vscode, é aí aonde os arquivos do seu projeto irão aparecer</p>
+        <p>Mas como ainda não temos nenhum arquivo aberto, temos algumas opções para se seguir</p>
+        <img src="assets/t2-img2.png" class="imgs-t">
+        <p>Você pode abrir uma nova pasta clicando no botão "open folder" ou arrastar uma pasta ou arquivo do explorador de arquivos do seu computador para a área destacada em azul</p>
+        </br>
+        <p>Outra forma é clicando no campo "File" na barra de menu no canto superior direito</p>
+        <img src="assets/t2-img3.png" class="imgs-t">
+        <p>Nesse campo você pode criar um novo arquivo, abrir um arquivo existente, abrir uma pasta dentre diversas outras opções</p>
+        </br>
+        <p>A terceira forma de abrir um arquivo é na própria tela do centro do editor de texto aonde também estão as opções de criar um novo arquivo, abrir um arquivo existente e abrir uma pasta</p>
+        <img src="assets/t2-img4.png" class="imgs-t">
+        <p>A quarta e última forma, e a que eu mais utilizo, é a opção de "abrir com code"</p>
+        <p>Ao pressionar com o botão direito em uma pasta ou arquivo (segurando shift caso esteja no windows 11), é possivel escolher a opção "abrir com code", 
+            que abrirá seu arquivo ou pasta diretamente no visual studio code</p>
+        <p>mas essa opção só é acessível caso tenha sido habilidada na instalação do vscode</p>
+    </div>
+</main>
+</html>
