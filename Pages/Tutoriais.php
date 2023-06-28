ca3d345d587b9ded47b80d3fd276bf65d43a6600
BruGes <brunogui244@gmail.com>
2023-06-28 05:53:13 -0300

    att tutoriais links

--- Pages/Tutoriais.php
+++ Pages/Tutoriais.php
@@ -27,49 +27,49 @@
         <div class="htmlcss container">
             <h2 class="titulo">HTML/CSS - Aulas</h2>
             <ul class="lista-aulas">
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 1</p></li>
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 2</p></li>
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 3</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto1.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 1</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto2.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 2</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto3.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 3</p></li>
             </ul>
         </div>
         <div class="php container">
             <h2 class="titulo">PHP - Aulas</h2>
             <ul class="lista-aulas">
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 1</p></li>
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 2</p></li>
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 3</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto1.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 1</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto2.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 2</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto3.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 3</p></li>
             </ul>
         </div>
         <div class="javascript container">
             <h2 class="titulo">JS - Aulas</h2>
             <ul class="lista-aulas">
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 1</p></li>
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 2</p></li>
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 3</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto1.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 1</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto2.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 2</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto3.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 3</p></li>
             </ul>
         </div>
         <div class="react container">
             <h2 class="titulo">REACT - Aulas</h2>
             <ul class="lista-aulas">
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 1</p></li>
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 2</p></li>
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 3</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto1.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 1</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto2.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 2</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto3.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 3</p></li>
             </ul>
         </div>
         <div class="emberjs container">
             <h2 class="titulo">EMBERJS - Aulas</h2>
             <ul class="lista-aulas">
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 1</p></li>
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 2</p></li>
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 3</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto1.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 1</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto2.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 2</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto3.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 3</p></li>
             </ul>
         </div>
         <div class="laravel container">
             <h2 class="titulo">Laravel - Aulas</h2>
             <ul class="lista-aulas">
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 1</p></li>
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 2</p></li>
-                <li class="lista-aulas-link"><a href="#"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 3</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto1.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 1</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto2.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 2</p></li>
+                <li class="lista-aulas-link"><a href="vscode-tuto3.php"><img class="img-icon" src="assets/visual-studio-code-2019.png"></a><p>VSCODE<br>AULA 3</p></li>
             </ul>
         </div>
     </div>
