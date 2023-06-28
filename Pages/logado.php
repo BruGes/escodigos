c5b40c85c0fc34a09301aecba0316f776ec47570
ihriel mosko <ihriel.mosko@gmail.com>
2023-06-28 12:05:00 -0300

    webhost bugfix

--- Pages/logado.php
+++ Pages/logado.php
@@ -1,3 +1,4 @@
+
 <?php include "consultas.php"; ?>
 
 <!DOCTYPE html>
@@ -12,8 +13,8 @@
 <header>
     <div class="container-elementos-header">
         <div class="logo-container">
-            <a href="../index.html"><h1 class="up-text">Escola</h1></a>
-            <a href="../index.html"><p class="down-text">do Código</p></a>
+            <a href="../index.php"><h1 class="up-text">Escola</h1></a>
+            <a href="../index.php"><p class="down-text">do Código</p></a>
         </div>
         <nav>
             <ul class="lista-navegação">
