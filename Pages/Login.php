fc60ab25089868871e6347ee01226caba8043684
ihriel mosko <ihriel.mosko@gmail.com>
2023-06-29 20:37:10 -0300

    login fix

--- Pages/Login.php
+++ Pages/Login.php
@@ -1,5 +1,4 @@
-<? session_start();
-include "../script/validation.php"?>
+<?php session_start(); ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -23,7 +22,7 @@ include "../script/validation.php"?>
                 <span class="span-login hidden">Email Incorreto</span>
             </div>
             <div class="password-container">
-                <input type="password" placeholder="Senha" id="password-login" name="Senha-login" class="campo senha" required>
+                <input type="password" placeholder="Senha" id="password-login" name="senha-login" class="campo senha" required>
                 <span class="span-login hidden">Senha Incorreta</span>
             </div>
             <a href="cadastro.php" class="btn cadastro">Cadastre-se</a>
@@ -35,3 +34,50 @@ include "../script/validation.php"?>
     
 </main>
 </html>
+
+<?php
+
+if (isset($_POST['logar'])) {
+    $email = $_POST['email-login'];
+    $senha = $_POST['senha-login'];
+
+    include "../script/config.php";
+    $consulta = $conn->prepare('SELECT * FROM `usuario` WHERE `ds_email` = :email AND `vl_senha` = :senhal');
+    $consulta->bindValue(':email',$email);
+    $consulta->bindValue(':senhal',$senha);
+    $consulta->execute();
+
+    $idconsulta = $conn->prepare('SELECT `id_usuario` FROM `usuario` WHERE `ds_email` = :emaill AND `vl_senha` = :senhal');
+    $idconsulta->bindValue(':emaill',$email);
+    $idconsulta->bindValue(':senhal',$senha);
+    $idconsulta->execute();
+    $iduser = $idconsulta->fetchColumn();
+
+
+    if($consulta->rowCount()==0){
+        ?>
+        <script>
+            window.location.replace("loginErrado.php");
+            //window.location.replace("https://siteteste69.000webhostapp.com/pages/loginErrado.php");
+        </script>
+        <?php
+    }
+    else{
+
+        $_SESSION['id'] = $iduser;
+
+        ?>
+        <script>
+            window.location.replace("Perfil.php");
+            //window.location.replace("https://siteteste69.000webhostapp.com/Perfil.php");
+        </script>
+        <?php
+        
+    }
+
+
+
+}
+    
+
+?>
